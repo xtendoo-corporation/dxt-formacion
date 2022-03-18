55258ad5394e38040855a02e0fba283de634953b
manuelcalerosolis <manuelcalerosolis@gmail.com>
2022-03-18 10:48:35 +0100

    vistas custom dxt

---
+++ dxt_crm_kamban_view/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "DXT CRM Kamban View",
+    "summary": """
+        Modify views kamban""",
+    "version": "15.0",
+    "depends": ["crm"],
+    "maintainers": ["dariocruzmauro"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": ["views/crm_lead_view.xml"],
+    "installable": True,
+    "auto_install": True,
+}
