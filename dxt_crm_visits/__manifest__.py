8c6b8cccd1dcb85d7158c8584e4122dc458daccb
manuelcalerosolis <manuelcalerosolis@gmail.com>
2022-03-21 13:06:48 +0100

    Módulo visitas dxt

---
+++ dxt_crm_visits/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "DXT CRM View",
+    "summary": """
+        Modify views CRM""",
+    "version": "15.0",
+    "depends": ["crm"],
+    "maintainers": ["dariocruzmauro"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": ["views/crm_lead_view.xml"],
+    "installable": True,
+    "auto_install": True,
+}
+
